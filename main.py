2ee8b57d83dc4bdab95d35cb623ad562182bd073
FarshadInspiration <39904310+FarshadInspiration@users.noreply.github.com>
2024-01-18 17:31:54 +0100

    Adding Comments

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import nltk
 sentence = "The quick brown fox jumps over the lazy dog."
 
 # Tokenize the sentence
+# Sentences to the words
 tokens = nltk.word_tokenize(sentence)
 
 # Perform POS tagging
+# Assigning roles to each word in a sentence
 tagged = nltk.pos_tag(tokens)
 
 # Print all adjectives
 adjectives = [word for word, pos in tagged if pos in ['JJ', 'JJR', 'JJS']]
 print(adjectives)
 
+print(tokens)
